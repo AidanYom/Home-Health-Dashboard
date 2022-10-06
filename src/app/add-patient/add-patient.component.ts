bd1f64d1ac24db953e4463bbe5a336acdf4a94fd
Jun0610 <lim321@purdue.edu>
2022-10-06 15:00:10 -0400

    add patient

--- src/app/add-patient/add-patient.component.ts
+++ src/app/add-patient/add-patient.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { PatientService } from '../services/patient.service';
 
 @Component({
   selector: 'app-add-patient',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPatientComponent implements OnInit {
 
-  constructor() { }
+  constructor(private patientService : PatientService, private router : Router) { }
 
   first_name = '';
   last_name = '';
@@ -16,14 +18,23 @@ export class AddPatientComponent implements OnInit {
   street: string = '';
   city: string = '';
   zip: string = '';
-  
+  nurse: string = '';
+  treatment: string = '';
   
 
   ngOnInit(): void {
   }
 
   addPatient(): void {
-
+    this.patientService.addPatients(-1, this.first_name, this.last_name, this.email, this.phone, "9/9/9999", "9/9/9999", "9/9/9999", this.street, this.city, this.zip, "Lebron James", "sickness").subscribe(
+      data => {
+        if (data.status == 200) {
+          console.log("success!")
+        }
+      }
+    )
+    this.router.navigateByUrl('admin');  
   }
 
 }
+
