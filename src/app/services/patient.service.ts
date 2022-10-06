b42f659780944964da25df63f52aa49c5b4a32d8
mbhasin1 <mbhasin@purdue.edu>
2022-10-05 20:58:53 -0400

    patient service done

---
+++ src/app/services/patient.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientService {
+  baseURL: string = "http://localhost:3001/";
+  constructor(private http: HttpClient) {}
+
+  addPatients(patientid: number, fname: string, lname: string, email: string, phone: string, dob: string, startDate:string,
+     endDate:string,  street:string, city:string, zip:string, assignedNurse:string, treatmentDesc: string): Observable<any> {
+
+    const headers = { 'content-type': 'application/json'}
+    const body = {
+      "patientid" : patientid,
+      "fname" : fname,
+      "lname" : lname,
+      "dob" : dob,
+      "email" : email,
+      "phone" : phone,
+      "startDate" : startDate,
+      "endData" : endDate,
+      "street" : street,
+      "city": city,
+      "zip" : zip,
+      "assignedNurse" : assignedNurse,
+      "treatmentDesc" : treatmentDesc
+
+
+    }
+    
+
+    return this.http.post(this.baseURL + 'new-patient', body,{'headers':headers})
+
+  }
+}
